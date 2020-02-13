0309bff50a77f634fc683a21a346f496b326fe3c
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-02-13 17:55:19 +0900

    add DbProfile

---
+++ src/DbProfile.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BEAR\Dev;
+
+use Aura\Sql\ExtendedPdoInterface;
+use Aura\Sql\Profiler;
+use Psr\Log\LoggerInterface;
+use Ray\Di\InjectorInterface;
+
+final class DbProfile
+{
+    /**
+     * @var ExtendedPdoInterface
+     */
+    private $pdo;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(InjectorInterface $injector)
+    {
+        $this->pdo = $injector->getInstance(ExtendedPdoInterface::class);
+        $this->pdo->setProfiler(new Profiler);
+        $this->pdo->getProfiler()->setActive(true);
+        $this->logger = $injector->getInstance(LoggerInterface::class);
+    }
+
+    /**
+     * @return array<string>
+     */
+    public function log() : array
+    {
+        $profiles = $this->pdo->getProfiler()->getProfiles();
+        foreach ($profiles as &$profile) {
+            unset($profile['trace'], $profile['duration']);
+        }
+        unset($profile);
+        if ($profiles) {
+            $this->logger->debug('sql:', $profiles);
+        }
+
+        return $profiles;
+    }
+}
---
+++ tests/DbProfileTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BEAR\Dev;
+
+use Aura\Sql\ExtendedPdo;
+use Aura\Sql\ExtendedPdoInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Ray\Di\AbstractModule;
+use Ray\Di\Injector;
+
+class DbProfileTest extends TestCase
+{
+    /**
+     * @var DbProfile
+     */
+    private $dbProfile;
+
+    protected function setUp() : void
+    {
+        $injector = new Injector(new class extends AbstractModule {
+            protected function configure() : void
+            {
+                $this->bind(ExtendedPdoInterface::class)->toInstance(new ExtendedPdo('sqlite::memory:'));
+                $this->bind(LoggerInterface::class)->toInstance(new NullLogger);
+            }
+        });
+        $this->dbProfile = new DbProfile($injector);
+    }
+
+    public function testTearDown() : void
+    {
+        $profiles = $this->dbProfile->log();
+        $this->assertIsArray($profiles);
+    }
+}
