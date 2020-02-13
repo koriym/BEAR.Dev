40d5e2d08b3bda1e7669618e3c74dd31c6a12f5c
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-02-13 18:28:07 +0900

    add Integration test demo

---
+++ demo/tests/Integration/DemoTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyProject\Inetgration;
+
+use BEAR\Dev\Http\HttpResourceClient;
+use BEAR\Dev\Http\HttpServiceTrait;
+use MyVendor\MyProject\Workflow\DemoTest as Workflow;
+
+class DemoTest extends Workflow
+{
+    use HttpServiceTrait;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $token = '_secret_token_';
+        $_SERVER['Authorization'] = $token;
+        $logFile = sprintf('%s/http_log/%s.log', __DIR__, __CLASS__);
+        $this->resource = new HttpResourceClient($this->injector, $logFile);
+    }
+
+    public function testIndex()
+    {
+        $index = $this->resource->get('http://127.0.0.1:8088/');
+        $this->assertSame(200, $index->code);
+
+        return $index;
+    }
+}
