96900caaba54653a67650fb2ded47bdff65c4092
koriym <akihito.koriyama@gmail.com>
2020-07-02 13:28:57 +0900

    update template files

--- template/tests/Http/WorkflowTest.php
+++ template/tests/Http/WorkflowTest.php
@@ -6,20 +6,20 @@ namespace BEAR\Skeleton\Http;
 
 use BEAR\Dev\Http\BuiltinServerStartTrait;
 use BEAR\Resource\ResourceObject;
-use BEAR\Skeleton\Hypermedia\HypermediaTest as Workflow;
+use BEAR\Skeleton\Hypermedia\WorkflowTest as Workflow;
 use BEAR\Skeleton\Injector;
 
-class HypermediaTest extends Workflow
+class WorkflowTest extends Workflow
 {
     use BuiltinServerStartTrait;
 
-    protected function setUp() : void
+    protected function setUp(): void
     {
         $_SERVER['Authorization'] = '_secret_token_';
-        $this->resource = $this->getHttpResourceClient(Injector::getInstance('app'), __CLASS__);
+        $this->resource = $this->getHttpResourceClient(Injector::getInstance('app'), self::class);
     }
 
-    public function testIndex() : ResourceObject
+    public function testIndex(): ResourceObject
     {
         $index = $this->resource->get($this->httpHost . '/');
         $this->assertSame(200, $index->code);
