93a78f7676a04e3e0daa8444815b462e8746e9b6
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-01-28 10:58:11 +0900

    Update DbProfile for aura/sql 3

--- tests/Db/DbProfileTest.php
+++ tests/Db/DbProfileTest.php
@@ -7,17 +7,21 @@ namespace BEAR\Dev\Db;
 use Aura\Sql\ExtendedPdo;
 use Aura\Sql\ExtendedPdoInterface;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\AbstractLogger;
 use Psr\Log\LoggerInterface;
-use Psr\Log\NullLogger;
 use Ray\Di\AbstractModule;
 use Ray\Di\Injector;
+use Ray\Di\InjectorInterface;
 
 class DbProfileTest extends TestCase
 {
-    /**
-     * @var DbProfile
-     */
-    private $dbProfile;
+    /** @var array<int, array<string, mixed>> */
+    public static $log;
+    /** @var ExtendedPdoInterface */
+    private $pdo;
+
+    /** @var InjectorInterface */
+    private $injector;
 
     protected function setUp() : void
     {
@@ -25,15 +29,29 @@ class DbProfileTest extends TestCase
             protected function configure() : void
             {
                 $this->bind(ExtendedPdoInterface::class)->toInstance(new ExtendedPdo('sqlite::memory:'));
-                $this->bind(LoggerInterface::class)->toInstance(new NullLogger);
+                $this->bind(LoggerInterface::class)->toInstance(
+                    new class extends AbstractLogger {
+                        public function log($level, $message, array $context = [])
+                        {
+                            DbProfileTest::$log[] = $context;
+                        }
+                    }
+                );
+                $this->install(new DevPdoModule());
             }
         });
-        $this->dbProfile = new DbProfile($injector);
+        $this->pdo = $injector->getInstance(ExtendedPdoInterface::class);
+        $this->injector = $injector;
     }
 
-    public function testTearDown() : void
+    public function testLog()
     {
-        $profiles = $this->dbProfile->log();
-        $this->assertIsArray($profiles);
+        new DbProfile($this->injector); // Start SQL log
+        $this->pdo->exec(/** @lang sql */'CREATE TABLE user(name, age)');
+        $this->pdo->perform(/** @lang sql */'INSERT INTO user (name, age) VALUES (?, ?)', ['koriym', 18]);
+        $this->assertCount(3, self::$log);
+        $this->assertSame('connect', self::$log[0]['function']);
+        $this->assertSame('exec', self::$log[1]['function']);
+        $this->assertSame('perform', self::$log[2]['function']);
     }
 }
