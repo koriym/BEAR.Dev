60f9ca1c8ed04f7e4d437f85cea1aa9cd01db226
koriym <akihito.koriyama@gmail.com>
2021-05-10 21:18:29 +0900

    Update setup.php

--- bin/setup.php
+++ bin/setup.php
@@ -3,7 +3,7 @@
 $appDir = dirname(__DIR__);
 passthru(sprintf('cd %s/tests/Fake/app', $appDir)
     . ' && composer config scripts.bin "echo installing"'
-    . ' && composer install --no-dev'
+    . ' && composer update --no-dev'
 );
 
 echo 'setup done.' . PHP_EOL;
