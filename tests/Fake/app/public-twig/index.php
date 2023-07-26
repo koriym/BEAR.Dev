2aad994d4495d00896b7f682e2abccbf6764bf0f
Akihito Koriyama <akihito.koriyama@gmail.com>
2023-07-26 09:37:56 +0900

    fixup! Find twig template

--- tests/Fake/app/public-twig/index.php
+++ tests/Fake/app/public-twig/index.php
@@ -7,4 +7,4 @@ use MyVendor\MyProject\Bootstrap;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 require dirname(__DIR__, 4) . '/vendor/autoload.php';
-exit((new Bootstrap())(PHP_SAPI === 'cli-server' ? 'dev-twig-app' : 'dev-prod-twig-app', $GLOBALS, $_SERVER));
+exit((new Bootstrap())(PHP_SAPI === 'cli-server' ? 'dev-html-app' : 'dev-prod-twig-app', $GLOBALS, $_SERVER));
